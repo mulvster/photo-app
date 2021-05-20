ce3dd1e0db9759cccd985e9b4b46300333f35ad8
April Mulvey <april.mulvey@gmail.com>
2021-05-20 16:45:20 -0700

    Setting up stuff to make api call for photos onSubmit

--- src/SearchPhotos.js
+++ src/SearchPhotos.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react"
 import { useForm } from "react-hook-form"
+import Unsplash, { toJson } from "unsplash-js";
+
+const unsplash =  new Unsplash({
+  accessKey: "ZrXHdFovpKQwQMTMGzPgSaf-Y1c0cUEpes5zECCgFMo"
+})
 
 const SearchPhotos = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm()
   const [formValue, setFormValue] = useState('')
-  console.log("Form value here",formValue)
 
-  const onSubmit = () => {
-    console.log("SUBMIT")
-  }
-  
+  const searchPhotos = async (e) => {
+    console.log("Submitting the Form")
+    e.preventDefault();
+  };
+
   return (
     <>
-      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <form className="form" onSubmit={handleSubmit(searchPhotos)}>
       <label className="label" htmlFor="query"> 
           📷
       </label>
@@ -25,7 +30,9 @@ const SearchPhotos = () => {
           value={formValue}
           onChange={(ev) => setFormValue(ev.target.value)}
          />
-        <button className="button" onClick={() => (console.log('clicky'))}>Submit</button>
+        <button type="submit" className="button">
+          Search
+        </button>
       </form>
     </>
   )
