d5cd50256913d260219177f48aa3cf7dcd6e2532
April Mulvey <april.mulvey@gmail.com>
2021-05-20 16:17:23 -0700

    Added onchange to input, capturing each keystroke inside input

--- src/SearchPhotos.js
+++ src/SearchPhotos.js
@@ -1,10 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import { useForm } from "react-hook-form"
 
 const SearchPhotos = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm()
-  const onSubmit = data => console.log("HI",data)
-  console.log(watch("example"))
+  const [formValue, setFormValue] = useState('')
+  console.log("Form value here",formValue)
+
+  const onSubmit = () => {
+    console.log("SUBMIT")
+  }
   
   return (
     <>
@@ -18,6 +22,8 @@ const SearchPhotos = () => {
           className="input"
           {...register("exampleRequired", { required: true })} 
           placeholder={`Try "space" or "cat"`}
+          value={formValue}
+          onChange={(ev) => setFormValue(ev.target.value)}
          />
         <button className="button" onClick={() => (console.log('clicky'))}>Submit</button>
       </form>
